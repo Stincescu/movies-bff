862830315e3b826fe064290bffd3eb0508fce3e1
Stincescu <sebistincescu@gmail.com>
2023-01-09 19:48:11 +0200

    init

---
+++ movies-bff/src/main/java/moviesbff/MoviesBffApplication.java
@@ -0,0 +1,13 @@
+package moviesbff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesBffApplication.class, args);
+	}
+
+}
---
+++ movies-bff/src/test/java/moviesbff/MoviesBffApplicationTests.java
@@ -0,0 +1,13 @@
+package moviesbff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
